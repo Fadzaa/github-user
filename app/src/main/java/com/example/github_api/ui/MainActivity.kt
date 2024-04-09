becf0259e040ebc7341f8e3eb5cb67565cc987e2
Fadzaa <fadza20@gmail.com>
2024-04-10 01:09:22 +0700

    add heading section, searchbar & searchview

--- app/src/main/java/com/example/github_api/ui/MainActivity.kt
+++ app/src/main/java/com/example/github_api/ui/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.github_api.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.example.github_api.R
+import com.example.github_api.data.response.DetailUserResponse
 
 import com.example.github_api.databinding.ActivityMainBinding
 import com.example.github_api.viewmodel.MainViewModel
@@ -23,14 +27,40 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         mainViewModel.user.observe(this) {
-            binding.tvUsername.text = it.login
+            setCurrentUser(it)
         }
 
         mainViewModel.isLoading.observe(this) {
             binding.progressBar.visibility = if (it) View.VISIBLE else View.GONE
         }
 
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener{ textView, actionId, event ->
+                    searchBar.text = searchView.text
+                    searchView.hide()
+                    Toast.makeText(this@MainActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
 
+
+    }
+
+    private fun setCurrentUser(detailUserResponse: DetailUserResponse) {
+        val followers = detailUserResponse.followers
+        val following = detailUserResponse.following
+        val repository = detailUserResponse.publicRepos
+
+        binding.tvUserFullname.text = detailUserResponse.name
+        binding.tvFollowers.text = getString(R.string.followers, followers)
+        binding.tvFollowings.text = getString(R.string.followings, following)
+        binding.tvPublicRepo.text = getString(R.string.public_repo, repository)
+        Glide.with(this)
+            .load(detailUserResponse.avatarUrl)
+            .into(binding.civCurrentUser)
     }
 
 
